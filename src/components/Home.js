665ed7ee9f212936a46c94c2ad29e7f849914668
johanlatorrre <johanlatorre621f@gmail.com>
2022-12-15 19:04:42 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Hola desde casa</h1>
+        </div>
+    );
+  }
+  
+  export default Home;
+  
---
+++ src/components/Template.js
@@ -0,0 +1,12 @@
+//import { Outlet } from 'react-router-dom';
+
+function Template() {
+  return (
+    <div>
+      <h1>la aplicacion 1234</h1>
+      
+    </div>
+  );
+}
+
+export default Template;
